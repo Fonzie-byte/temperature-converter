c382d1779a5ba09a6ee61bb94c432c747cdf8a6d
Fonzie-byte <twistfons@live.nl>
2025-06-29 02:08:36 +0200

    [Code] Consistent code styling

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ use std::fmt;
 /// assert_eq!(from_fahrenheit(-40.0), -40.0);
 /// ```
 pub fn from_fahrenheit(fahrenheit: f64) -> f64 {
-    5.0 / 9.0 * (fahrenheit - 32.0)
+    (fahrenheit - 32.0) / 1.8
 }
 
 /// Converts the given degrees Celsius to Fahrenheit and returns it as a float.
@@ -19,6 +19,7 @@ pub fn from_fahrenheit(fahrenheit: f64) -> f64 {
 /// # Examples
 /// ```
 /// use temperature_converter::from_celsius;
+/// 
 /// assert_eq!(from_celsius(100.0), 212.0);
 /// assert_eq!(from_celsius(0.0), 32.0);
 /// assert_eq!(from_celsius(-40.0), -40.0);
